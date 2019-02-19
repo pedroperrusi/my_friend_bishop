68c4e24fcc2be54dea70bce37b0489b650b8d514
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-02-19 04:28:29 +0100

    Added a reasonable main

--- app/main.cc
+++ app/main.cc
@@ -18,8 +18,19 @@
  */
 int main()
 {
-    chess::Bishop friendBishop;
+    size_t initial_x = 3;
+    size_t initial_y = 3;
 
+    std::cout << "The considered start position is (" << initial_x << ", " << initial_y << ")" << std::endl;
+
+    size_t final_x = 6;
+    size_t final_y = 6;
+
+    std::cout << "The considered ending position is (" << final_x << ", " << final_y << ")" << std::endl;
+
+    chess::Bishop friendBishop(initial_x, initial_y);
+
+    std::cout << "As expected, the position is reachable in " << friendBishop.reachableInHowManySteps(final_x, final_y) << " chess movements" << std::endl;
 
     return EXIT_SUCCESS;
 }
