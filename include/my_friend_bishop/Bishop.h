9b29ea254f1f4e40fa7a6866c9cdbb5a5fd8577f
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-02-19 01:00:13 +0100

    Created setPosition to provide an more fluid interface apart from contructors

--- include/my_friend_bishop/Bishop.h
+++ include/my_friend_bishop/Bishop.h
@@ -24,76 +24,85 @@ const size_t CHESS_BOARD_HIGHT = 8;
  */
 class Bishop
 {
-  public:
-    // Class constructor
-    Bishop()
-    {
-        x_pos = 0;
-        y_pos = 0;
-        idx_pos = 0;
-    };
-    // set initial 2D position on constructor
-    Bishop(size_t x_pos, size_t y_pos)
-    {
-        setX(x_pos);
-        setY(y_pos);
-        setIdxFromXY();
-    };
-    // set initial idx vector position on constructor
-    Bishop(size_t idx_pos)
-    {
-        setIdx(idx_pos);
-        setXYFromIdx();
-    };
-    // Class destructor
-    ~Bishop()
-    {
-        
-    };
+public:
+  // Class constructor
+  Bishop()
+  {
+    x_pos = 0;
+    y_pos = 0;
+    idx_pos = 0;
+  };
+  // set initial 2D position on constructor
+  Bishop(size_t x_pos, size_t y_pos)
+  {
+    setPosition(x_pos, y_pos);
+  };
+  // set initial idx vector position on constructor
+  Bishop(size_t idx_pos)
+  {
+    setPosition(idx_pos);
+  };
+  // Class destructor
+  ~Bishop(){
 
-    inline size_t getX(){ return x_pos; };
+  };
 
-    inline void setX(size_t x_pos){ this->x_pos = x_pos; }
+  inline void setPosition(size_t x_pos, size_t y_pos)
+  {
+    setX(x_pos);
+    setY(y_pos);
+    setIdxFromXY();
+  }
 
-    inline size_t getY(){ return y_pos; };
+   inline void setPosition(size_t idx_pos)
+  {
+    setIdx(idx_pos);
+    setXYFromIdx();
+  };
 
-    inline void setY(size_t y_pos){ this->y_pos = y_pos; }
+  inline size_t getX() { return x_pos; };
 
-    inline size_t getIdx(){ return idx_pos; };
+  inline void setX(size_t x_pos) { this->x_pos = x_pos; }
 
-    inline void setIdx(size_t idx_pos){ this->idx_pos = idx_pos; }
+  inline size_t getY() { return y_pos; };
+
+  inline void setY(size_t y_pos) { this->y_pos = y_pos; }
+
+  inline size_t getIdx() { return idx_pos; };
+
+  inline void setIdx(size_t idx_pos) { this->idx_pos = idx_pos; }
 
 private:
-    /**
+  /**
      * Coordinates for a chess piece in different representations
      */
-    // X position in a 2D grid
-    size_t x_pos;
-    // Y position in a 2D grid
-    size_t y_pos;
-    // index position in a 1D vector
-    size_t idx_pos;
+  // X position in a 2D grid
+  size_t x_pos;
+  // Y position in a 2D grid
+  size_t y_pos;
+  // index position in a 1D vector
+  size_t idx_pos;
 
-    /**
+  /**
      * @brief Set the Idx From 2D X and Y coordibates
      * 
      */
-    inline void setIdxFromXY()
-    {
-        idx_pos = x_pos + y_pos * CHESS_BOARD_WIDTH;
-    }
+  inline void setIdxFromXY()
+  {
+    idx_pos = x_pos + y_pos * CHESS_BOARD_WIDTH;
+  }
 
-    /**
+  /**
      * @brief Set X and Y coordinates based on vectorial idx
      * 
      */
-    inline void setXYFromIdx()
-    {
-        // X position is the modulo
-        x_pos = idx_pos % CHESS_BOARD_WIDTH;
-        // Y position is the integer division
-        y_pos = idx_pos / CHESS_BOARD_WIDTH;
-    }
+  inline void setXYFromIdx()
+  {
+    // X position is the modulo
+    x_pos = idx_pos % CHESS_BOARD_WIDTH;
+    // Y position is the integer division
+    y_pos = idx_pos / CHESS_BOARD_WIDTH;
+  }
 };
 } // namespace chess
 #endif // _BISHOP_H_
